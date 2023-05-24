8f950391b6a7b8febb104b6a8578493168b26e72
foosaan <fauzan4297@gmail.com>
2023-05-24 11:20:58 +0700

    membuat akses modofier public

--- paradigmaOOP/paradigmaOOP.cpp
+++ paradigmaOOP/paradigmaOOP.cpp
@@ -20,4 +20,12 @@ public:
 };
 
 class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dihapus\n" << endl;
+	}
 };
